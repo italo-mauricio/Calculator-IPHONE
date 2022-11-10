b0f69af8a68a25fde1c7d4ea3befa7c954c95e32
italo <italogithub@gmail.com>
2022-11-10 10:14:38 -0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const numbers = document.querySelectorAll('.numbers');
-const result = document.querySelector('.result');
+const result = document.querySelector('.result span');
+const signs = document.querySelectorAll('.sign');
+const equals = document.querySelector('equals');
+const clear = document.querySelector('.negative');
+const percent = document.querySelector('.percent')
 
 
 let firstValue = "";
@@ -10,11 +14,13 @@ let sign = "";
 let resultValue = 0;
 
 for (let i=0; i < numbers.length; i++){
-
     numbers[i].addEventListener('click', (e) => {
         let atr = e.target.getAttribute('value');
         if(isFirstValue === false) {
             getFirstValue(atr)
+        }
+        if(isSecondValue == false){
+            getFirstValue(atr);
         }
     })
 }
@@ -25,3 +31,37 @@ function getFirstValue(el){
     result.innerHTML = firstValue;
     firstValue = +firstValue;
 }
+
+function getSecondValue(el){
+    if (firstValue != "" && sign != ""){
+        secondValue += el;
+        result.innerHTML = secondValue;
+        secondValue = +secondValue;
+    }
+}
+
+function getSign(){
+    for(let i = 0; i < signs.length; i++ ){
+        signs[i].addEventListener('click', (e) =>{
+            sign = e.target.getAttribute('value');
+            isFirstValue = true;
+        })
+    }
+}
+
+getSign();
+
+equals.addEventListener('click', ()=> {
+    result.innerHTML = "";
+    if (sign === "+") {
+        resultValue = firstValue + secondValue;
+
+    }else if (sign === "-") {
+        resultValue = firstValue - secondValue;
+    }else if (sign === "x") {
+        resultValue = firstValue * secondValue;
+    }else if (sign === "/") {
+        resultValue = firstValue / secondValue;
+    }
+    result.innerHTML = resultValue;
+})
